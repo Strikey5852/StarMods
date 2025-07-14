8815814458233678bc8d4344f10e3c007ecc1d83
Strikey5852 <strikinglegend5852@gmail.com>
2025-07-14 15:42:59 +0530

    Initial commit

---
+++ ImprovedGlowSquids/src/main/java/net/strikey/improvedglowsquids/ImprovedGlowSquids.java
@@ -0,0 +1,15 @@
+package net.strikey.improvedglowsquids;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ImprovedGlowSquids implements ModInitializer {
+	public static final String MOD_ID = "improvedglowsquids";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ ImprovedGlowSquids/src/main/java/net/strikey/improvedglowsquids/ImprovedGlowSquidsClient.java
@@ -0,0 +1,12 @@
+package net.strikey.improvedglowsquids;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImprovedGlowSquidsClient implements ClientModInitializer {
+    public static final String MOD_ID = "improvedglowsquids";
+
+    @Override
+    public void onInitializeClient() {
+    }
+
+}
