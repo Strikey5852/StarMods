a2de20314621255ce90eeea01b1d41e9131ad833
Strikey5852 <strikinglegend5852@gmail.com>
2025-07-17 17:27:56 +0530

    Made Banners Unglow

--- ImprovedGlowSquids/src/main/java/net/strikey/improvedglowsquids/event/ModEvents.java
+++ ImprovedGlowSquids/src/main/java/net/strikey/improvedglowsquids/event/ModEvents.java
@@ -1,19 +1,17 @@
 package net.strikey.improvedglowsquids.event;
 
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
-import net.minecraft.block.BannerBlock;
-import net.minecraft.block.entity.BannerBlockEntity;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BannerBlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Items;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-
-import net.strikey.improvedglowsquids.util.GlowingBanner;  // Import the interface
+import net.strikey.improvedglowsquids.util.GlowingBanner;
 
 public class ModEvents {
 
@@ -22,7 +20,8 @@ public class ModEvents {
             BlockPos pos = hitResult.getBlockPos();
             BlockState state = world.getBlockState(pos);
 
-            if (!(state.getBlock() instanceof BannerBlock)) {
+            var blockEntity = world.getBlockEntity(pos);
+            if (!(blockEntity instanceof BannerBlockEntity bannerEntity)) {
                 return ActionResult.PASS;
             }
 
@@ -30,24 +29,35 @@ public class ModEvents {
 
             if (heldItem.getItem() == Items.GLOW_INK_SAC) {
                 if (!world.isClient) {
-                    // Get the block entity at the banner position
-                    var blockEntity = world.getBlockEntity(pos);
-                    if (blockEntity instanceof BannerBlockEntity bannerEntity && blockEntity instanceof GlowingBanner glowingBanner) {
+                    if (blockEntity instanceof GlowingBanner glowingBanner) {
 
-                        // Toggle glowing state
-                        boolean currentlyGlowing = glowingBanner.improvedGlowSquids$isGlowing();
-                        if (currentlyGlowing) {
+                        if (glowingBanner.improvedGlowSquids$isGlowing()) {
                             return ActionResult.PASS;
                         }
+
                         glowingBanner.improvedGlowSquids$setGlowing(true);
-
-                        // Mark block entity dirty so data saves
                         bannerEntity.markDirty();
-
-                        // Notify clients of block update to refresh rendering
                         world.updateListeners(pos, state, state, 3);
                     }
-                    // Consume one glow ink sac from player hand
+                    if (!player.isCreative()) {
+                        heldItem.decrement(1);
+                    }
+                }
+                return ActionResult.SUCCESS;
+            }
+
+            if (heldItem.getItem() == Items.INK_SAC) {
+                if (!world.isClient) {
+                    if (blockEntity instanceof GlowingBanner glowingBanner) {
+
+                        if (!glowingBanner.improvedGlowSquids$isGlowing()) {
+                            return ActionResult.PASS;
+                        }
+
+                        glowingBanner.improvedGlowSquids$setGlowing(false);
+                        bannerEntity.markDirty();
+                        world.updateListeners(pos, state, state, 3);
+                    }
                     if (!player.isCreative()) {
                         heldItem.decrement(1);
                     }
